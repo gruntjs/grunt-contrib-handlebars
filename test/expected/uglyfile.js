13b3d16d0bb31f3645e31baaede87c2d8850a0d7
Tyler Kellen <tyler@sleekcode.net>
2012-09-17 08:44:15 -0500

    file with double quote in name breaks windows, remove it

--- test/expected/uglyfile.js
+++ test/expected/uglyfile.js
@@ -1,12 +1,5 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["test/fixtures/it\"s-a-bad-filename.hbs"] = function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  
-
-
-  return "Why would you name your file like this?";};
-
 this["JST"]["test/fixtures/it's-a-bad-filename.hbs"] = function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers;
   
