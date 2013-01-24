1e58b5dc99c20e4d3a2f9791c59804648348e4ad
Pete Hawkins <pete@phawk.co.uk>
2013-01-24 19:05:59 +0000

    Fix tests to what their meant to be

--- test/expected/amd_compile.js
+++ test/expected/amd_compile.js
@@ -2,12 +2,12 @@ define(function() {
 
 this["JST"] = this["JST"] || {};
 
-this["JST"]["test/fixtures/amd.html"] = function (Handlebars,depth0,helpers,partials,data) {
+this["JST"]["test/fixtures/amd.html"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers;
   
 
 
-  return "<section class=\"main-app\">\n    <h1>Some title</h1>\n    <p>I've been compiled with amd support</p>\n</section>";};
+  return "<section class=\"main-app\">\n    <h1>Some title</h1>\n    <p>I've been compiled with amd support</p>\n</section>";});
 
   return this["JST"];
 
--- test/expected/ns_nested_this.js
+++ test/expected/ns_nested_this.js
@@ -3,7 +3,7 @@ this["MyApp"]["JST"] = this["MyApp"]["JST"] || {};
 this["MyApp"]["JST"]["Main"] = this["MyApp"]["JST"]["Main"] || {};
 
 this["MyApp"]["JST"]["Main"]["test/fixtures/basic.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers; data = data || {};
+  helpers = helpers || Handlebars.helpers;
   
 
 
