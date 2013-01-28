2214b1270fea72009717a167985fef438f780d4e
kawanet <u-suke@kawa.net>
2013-01-28 20:48:10 +0900

    add processContent test and doc

---
+++ test/expected/processcontent.js
@@ -0,0 +1,8 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["test/fixtures/has-spaces.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div>\n<span>this template has many spaces</span>\n</div>";});
