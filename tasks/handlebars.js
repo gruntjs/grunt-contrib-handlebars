67860ae563cb0639bc5e0e2281e6d3faed68a8c8
Phil Dokas <phil@jetless.org>
2013-02-21 22:46:49 -0800

    Adds a partialsPathRegex option
    This option is useful for large codebases that organize partials into a subdirectory of their templates. When used in combination with a lax partialRegex filter, partials inside the specified subdirectory can leave off the leading _ in their filename.

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -41,6 +41,9 @@ module.exports = function(grunt) {
     if(options.namespace !== false){
       nsInfo = helpers.getNamespaceDeclaration(options.namespace);
     }
+    
+    // assign regex for partials directory detection
+    var partialsPathRegex = options.partialsPathRegex || /./;
 
     // assign regex for partial detection
     var isPartial = options.partialRegex || /^_/;
@@ -84,7 +87,7 @@ module.exports = function(grunt) {
         }
 
         // register partial or add template to namespace
-        if (isPartial.test(_.last(filepath.split('/')))) {
+        if (partialsPathRegex.test(filepath) && isPartial.test(_.last(filepath.split('/')))) {
           filename = processPartialName(filepath);
           partials.push('Handlebars.registerPartial('+JSON.stringify(filename)+', '+compiled+');');
         } else {
