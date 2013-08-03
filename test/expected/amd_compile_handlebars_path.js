359c65639ec9d5757639f5a4138eb217bf5db641
stereobit <t@stereob.it>
2013-08-03 17:16:54 +0100

    add missing expected test file

---
+++ test/expected/amd_compile_handlebars_path.js
@@ -0,0 +1,16 @@
+define(['lib/handlebars'], function(Handlebars) {
+
+this["JST"] = this["JST"] || {};
+
+this["JST"]["test/fixtures/amd.html"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+
+
+
+  return "<section class=\"main-app\">\n    <h1>Some title</h1>\n    <p>I've been compiled with amd support</p>\n</section>";
+  });
+
+return this["JST"];
+
+});
