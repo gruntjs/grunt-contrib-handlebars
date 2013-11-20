d4bc943a02b0d38d99910d041dcfee2e5b7ae5ef
Collin F <collin.forrester@gmail.com>
2013-11-20 13:16:09 -0600

    Adds ability to support RequireJS pragmas plus tests and windows tests support

---
+++ test/expected/amd_compile_pragmas.js
@@ -0,0 +1,30 @@
+//>>excludeStart("myPragmaBuildExclude", pragmas.myPragmaBuildExclude);
+
+
+define(['handlebars'], function(Handlebars) {
+
+
+ //>>excludeEnd("myPragmaBuildExclude");
+ (function(){
+
+this["JST"] = this["JST"] || {};
+
+this["JST"]["test/fixtures/amd.html"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<section class=\"main-app\">\r\n    <h1>Some title</h1>\r\n    <p>I've been compiled with amd support</p>\r\n</section>";
+  });
+
+return this["JST"];
+
+})();
+//>>excludeStart("myPragmaBuildExclude", pragmas.myPragmaBuildExclude);
+
+
+});
+
+
+ //>>excludeEnd("myPragmaBuildExclude");
