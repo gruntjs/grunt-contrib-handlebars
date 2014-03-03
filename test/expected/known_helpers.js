ce1f553a7efe263d8834e07d101904a78d856c1b
Tyler Kellen <tyler@sleekcode.net>
2014-03-03 10:32:14 -0500

    bump to handlebars 1.3

--- test/expected/known_helpers.js
+++ test/expected/known_helpers.js
@@ -3,7 +3,7 @@ this["JST"] = this["JST"] || {};
 this["JST"]["test/fixtures/uses_helpers.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, options, escapeExpression=this.escapeExpression, self=this, functionType="function", blockHelperMissing=helpers.blockHelperMissing;
+  var buffer = "", stack1, helper, options, escapeExpression=this.escapeExpression, self=this, functionType="function", blockHelperMissing=helpers.blockHelperMissing;
 
 function program1(depth0,data) {
   
@@ -14,10 +14,10 @@ function program1(depth0,data) {
   buffer += "This template uses custom helpers: "
     + escapeExpression(helpers['my-helper'].call(depth0, "variable", {hash:{},data:data}))
     + " & ";
-  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data};
-  if (stack1 = helpers['another-helper']) { stack1 = stack1.call(depth0, options); }
-  else { stack1 = (depth0 && depth0['another-helper']); stack1 = typeof stack1 === functionType ? stack1.call(depth0, options) : stack1; }
-  if (!helpers['another-helper']) { stack1 = blockHelperMissing.call(depth0, stack1, options); }
+  options={hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data}
+  if (helper = helpers['another-helper']) { stack1 = helper.call(depth0, options); }
+  else { helper = (depth0 && depth0['another-helper']); stack1 = typeof helper === functionType ? helper.call(depth0, options) : helper; }
+  if (!helpers['another-helper']) { stack1 = blockHelperMissing.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data}); }
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "!\n";
   return buffer;
