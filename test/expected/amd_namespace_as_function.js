9367a1660426df7f0b900dcf3640ffbcab99dc4a
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-11-02 22:35:55 +0100

    Add failing test for using namespace function with AMD

---
+++ test/expected/amd_namespace_as_function.js
@@ -0,0 +1,18 @@
+define(['handlebars', 'handlebars.helpers'], function(Handlebars) {
+
+this["JST"] = this["JST"] || {};
+this["JST"]["countries"] = this["JST"]["countries"] || {};
+
+this["JST"]["countries"]["basic"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "Basic template that does nothing.";
+  },"useData":true});
+
+this["JST"]["treeNav"] = this["JST"]["treeNav"] || {};
+this["JST"]["treeNav"]["leaves"] = this["JST"]["treeNav"]["leaves"] || {};
+
+this["JST"]["treeNav"]["leaves"]["basic"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "Basic template that does nothing.";
+  },"useData":true});
+
+return this["JST"];
+});
