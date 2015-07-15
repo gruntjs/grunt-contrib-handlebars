4a4a57b56c86cece0236945d67f57fb72e012264
iamwillburn <wiburn@gmail.com>
2015-07-15 11:05:47 +0100

    Updates handlebars.js
    Made changes to support the output of node-like (and brunch-like) modules. Use by setting the option 'node_like' to 'true'.

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -47,11 +47,12 @@ module.exports = function(grunt) {
     // In case only one namespace has been declared it will only return it.
     if (declarations.length === 1) {
       return declarations[0];
+    } else {
+      // we only need to take any declaration to extract the global namespace.
+      // Another option might be find the shortest declaration which is the global one.
+      var matches = declarations[0].match(/(this\[[^\[]+\])/g);
+      return matches[0];
     }
-    // We only need to take any declaration to extract the global namespace.
-    // Another option might be find the shortest declaration which is the global one.
-    var matches = declarations[0].match(/(this\[[^\[]+\])/g);
-    return matches[0];
   };
 
   grunt.registerMultiTask('handlebars', 'Compile handlebars templates and partials.', function() {
@@ -77,6 +78,8 @@ module.exports = function(grunt) {
     var processPartialName = options.processPartialName || defaultProcessPartialName;
     var processAST = options.processAST || defaultProcessAST;
     var useNamespace = options.namespace !== false;
+    // Adding a 'process' function so that we can wrap each individual file
+    // within a module.
 
     // assign compiler options
     var compilerOptions = options.compilerOptions || {};
@@ -100,13 +103,12 @@ module.exports = function(grunt) {
 
       // Just get the namespace info for a given template
       var getNamespaceInfo = _.memoize(function(filepath) {
-        if (!useNamespace) {
-          return undefined;
-        }
+        if (!useNamespace) {return undefined;}
         if (_.isFunction(options.namespace)) {
           return nsdeclare(options.namespace(filepath), nsDeclareOptions);
+        } else {
+          return nsdeclare(options.namespace, nsDeclareOptions);
         }
-        return nsdeclare(options.namespace, nsDeclareOptions);
       });
 
       // iterate files, processing partials and templates separately
@@ -115,8 +117,9 @@ module.exports = function(grunt) {
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
           return false;
+        } else {
+          return true;
         }
-        return true;
       })
       .forEach(function(filepath) {
         var src = processContent(grunt.file.read(filepath), filepath);
@@ -162,6 +165,11 @@ module.exports = function(grunt) {
             templates.push(nsInfo.namespace + '[' + JSON.stringify(filename) + '] = ' + compiled + ';');
           } else if (options.commonjs === true) {
             templates.push(compiled + ';');
+          } else if (options.node_like === true) {
+            var prefix = 'require.register("' + filename + '", function(exports, require, module) {\n'
+            + 'module.exports = ';
+            var suffix = '\n});'
+            templates.push(prefix + compiled + suffix);
           } else {
             templates.push(compiled);
           }
@@ -192,7 +200,7 @@ module.exports = function(grunt) {
           } else if (typeof options.amd === 'string') {
             output.unshift('define([\'' + options.amd + '\'], function(Handlebars) {');
           } else if (typeof options.amd === 'function') {
-            output.unshift('define([\'' + options.amd(filename, ast, compiled) + '\'], function(Handlebars) {');
+            output.unshift("define(['" + options.amd(filename, ast, compiled) + "'], function(Handlebars) {");
           } else if (Array.isArray(options.amd)) {
             // convert options.amd to a string of dependencies for require([...])
             var amdString = '';
@@ -220,7 +228,7 @@ module.exports = function(grunt) {
           if (useNamespace) {
             output.push('return ' + nsInfo.namespace + ';');
           }
-          // Export the templates object for CommonJS environments.
+
           output.unshift('module.exports = function(Handlebars) {');
           output.push('};');
         }
