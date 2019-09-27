15ab569dc84de8d8d73d832f20129ad1acdd0254
Chris Barth <chrisjbarth@hotmail.com>
2019-09-27 09:26:19 -0400

    Fix tests

--- test/expected/handlebarsnowrap.js
+++ test/expected/handlebarsnowrap.js
@@ -8,7 +8,7 @@ this["JST"]["test/fixtures/one.hbs"] = {"compiler":[7,">= 4.0.0"],"main":functio
     var stack1, helper;
 
   return "<p>Hello, my name is "
-    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext||{}),{"name":"name","hash":{},"data":data}) : helper)))
     + ". I live in "
     + ((stack1 = container.invokePartial(partials.partial,depth0,{"name":"partial","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
     + "</p>";
