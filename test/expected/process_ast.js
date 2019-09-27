c67f0cab312b1596710d9ebc2ff757687f4aaccc
XhmikosR <xhmikosr@gmail.com>
2019-09-27 17:11:42 +0300

    Update all the stuff.
    * handlebars to v4.3.4
    * chalk to v2.4.2
    * CI configs
    
    Minimum supported Node.js version is 8.x.

--- test/expected/process_ast.js
+++ test/expected/process_ast.js
@@ -1,9 +1,9 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["test/fixtures/one.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper;
+this["JST"]["test/fixtures/one.hbs"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=container.propertyIsEnumerable;
 
   return "<p>Hello, my name is "
-    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"name","hash":{},"data":data}) : helper)))
     + ". I live in Fooville</p>";
 },"useData":true});
--- test/expected/uglyfile.js
+++ test/expected/uglyfile.js
@@ -1,5 +1,5 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["test/fixtures/it's-a-bad-filename.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+this["JST"]["test/fixtures/it's-a-bad-filename.hbs"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     return "Why would you name your file like this?";
 },"useData":true});
