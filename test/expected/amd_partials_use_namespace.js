1a0aa8d4fb86f261ce90bf8557a335deedc3048d
Vlad Filippov <vlad.filippov@gmail.com>
2021-05-14 21:45:13 -0400

    Add Actions

--- test/expected/amd_partials_use_namespace.js
+++ test/expected/amd_partials_use_namespace.js
@@ -1,21 +1,26 @@
 define(['handlebars'], function(Handlebars) {
 
-this["JST"] = this["JST"] || {};
-
-Handlebars.registerPartial("partial", this["JST"]["partial"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<span>Canada</span>";
-},"useData":true}));
-
-this["JST"]["test/fixtures/one.hbs"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=container.propertyIsEnumerable;
-
-  return "<p>Hello, my name is "
-    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"name","hash":{},"data":data}) : helper)))
-    + ". I live in "
-    + ((stack1 = container.invokePartial(partials.partial,depth0,{"name":"partial","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + "</p>";
-},"usePartial":true,"useData":true});
-
-return this["JST"];
-
-});
+  this["JST"] = this["JST"] || {};
+  
+  Handlebars.registerPartial("partial", this["JST"]["partial"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+      return "<span>Canada</span>";
+  },"useData":true}));
+  
+  this["JST"]["test/fixtures/one.hbs"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+      var stack1, helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+          if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+            return parent[propertyName];
+          }
+          return undefined
+      };
+  
+    return "<p>Hello, my name is "
+      + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"name","hash":{},"data":data,"loc":{"start":{"line":1,"column":21},"end":{"line":1,"column":29}}}) : helper)))
+      + ". I live in "
+      + ((stack1 = container.invokePartial(lookupProperty(partials,"partial"),depth0,{"name":"partial","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+      + "</p>";
+  },"usePartial":true,"useData":true});
+  
+  return this["JST"];
+  
+  });
--- test/expected/unknown_helpers.js
+++ test/expected/unknown_helpers.js
@@ -3,12 +3,17 @@ this["JST"] = this["JST"] || {};
 this["JST"]["test/fixtures/uses_helpers.hbs"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     return "";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, options, alias1=container.propertyIsEnumerable, alias2=depth0 != null ? depth0 : (container.nullContext || {}), alias3=container.hooks.helperMissing, buffer = 
+    var stack1, helper, options, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    }, buffer = 
   "This template uses custom helpers: "
-    + container.escapeExpression((helpers["my-helper"]||(depth0 && depth0["my-helper"])||alias3).call(alias2,"variable",{"name":"my-helper","hash":{},"data":data}))
+    + container.escapeExpression((lookupProperty(helpers,"my-helper")||(depth0 && lookupProperty(depth0,"my-helper"))||alias2).call(alias1,"variable",{"name":"my-helper","hash":{},"data":data,"loc":{"start":{"line":1,"column":35},"end":{"line":1,"column":59}}}))
     + " & ";
-  stack1 = ((helper = (helper = helpers["another-helper"] || (depth0 != null ? depth0["another-helper"] : depth0)) != null ? helper : alias3),(options={"name":"another-helper","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data}),(typeof helper === "function" ? helper.call(alias2,options) : helper));
-  if (!helpers["another-helper"]) { stack1 = container.hooks.blockHelperMissing.call(depth0,stack1,options)}
+  stack1 = ((helper = (helper = lookupProperty(helpers,"another-helper") || (depth0 != null ? lookupProperty(depth0,"another-helper") : depth0)) != null ? helper : alias2),(options={"name":"another-helper","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":1,"column":62},"end":{"line":1,"column":100}}}),(typeof helper === "function" ? helper.call(alias1,options) : helper));
+  if (!lookupProperty(helpers,"another-helper")) { stack1 = container.hooks.blockHelperMissing.call(depth0,stack1,options)}
   if (stack1 != null) { buffer += stack1; }
   return buffer + "!\n";
 },"useData":true});
